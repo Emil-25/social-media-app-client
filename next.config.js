a56abda3b0ce4490aca9c2b337ae1fddbd5f1e99
emil-25 <emilrahimov25@gmail.com>
2023-11-06 18:34:37 +0400

    Add env to nextconfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  }
 };
 
 module.exports = nextConfig;
